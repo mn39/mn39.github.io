e5eea5cb4691f3476b56f90086ba244fe88877f1
mn39 <borissal07@naver.com>
2022-03-05 16:19:02 +0900

    very good

--- BABYAK/app.py
+++ BABYAK/app.py
@@ -1,12 +1,16 @@
-from flask import Flask
-import requests
-
+from flask import Flask, request
+ 
 app = Flask(__name__)
+ 
+@app.route('/param')
+def hello():
 
-@app.route('/test', methods=['POST'])
-def create():
-    print(request.is_json)
-    params = request.get_json()
-    print(params)
-    return 'ok'
+    a = request.args.get('pname')
+    b = request.args.get('possDate')
+    
+    return "thx"
+ 
 
+
+if __name__== "__main__":
+    app.run(port=80)
